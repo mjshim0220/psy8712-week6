9272ea8751f8b8576e59b0e70650041008c64dc1
MJ Shim <shim0220@umn.edu>
2024-02-27 15:20:42 -0600

    Version 1

--- R/week6.R
+++ R/week6.R
@@ -6,14 +6,21 @@ library(stringr)
 
 
 #Data Import
-citations<-stri_read_lines("../data/citations.txt", encoding="Windows-1252")
+citations<-stri_read_lines("../data/citations.txt", encoding="Windows-1252") #encoding=ISO-8859-1
 citations_txt<-citations[!stri_isempty(citations)]
 length(citations)-length(citations_txt)
 mean(nchar(citations_txt))
 
 #Data Cleaning
+sample(citations_txt,20)
 
 citations_tbl<-tibble(line=1:length(citations_txt),cite=citations_txt) %>% 
-  mutate(cite=str_replace_all(cite,"\"", ""))
-
+  mutate(cite=str_replace_all(cite, pattern="\"", "")) %>% 
+  mutate(year=str_extract_all(cite, pattern="(?<=\\()[12][0-9]{3}(?=\\))")) %>% 
+  #mutate(page_start=str_extract(cite, "")) # I was not able to complete line 20
+  mutate(perf_ref=str_detect(cite, regex("performance",ignore_case=TRUE))) %>% 
+  mutate(title=str_match(cite, pattern= "\\(\\d{4}\\)\\.\\s(.*?)\\.")[,2]) %>% 
+  mutate(first_author=str_match(cite, pattern="^([^,]+),")[,2])
+  
+sum(!is.na(citations_tbl$first_author))
          
